05cd77eb177a415e5b1b6b43831a613fc52fb5c5
0x401 <0x401@users.noreply.github.com>
2017-04-05 14:08:55 +0800

    Update ip.php

--- address list dns/ip.php
+++ address list dns/ip.php
@@ -3,6 +3,7 @@ header('Content-type: application/json');
 $ip = gethostbyname($_GET['domain']);
 $arr=array('ip'=>$ip);  
 $result=json_encode($arr);
-$callback=$_GET['callback'];  
-echo $callback."($result)"; 
+$callback=!empty($_GET['callback'])?$_GET['callback']:null;  
+if($callback){echo $callback."($result)"; }
+else echo $_GET['domain'].':'.$ip; 
 ?>
